22e20cbe541a0f28d695c0b43e5b9e06f00afc49
Andres Samaniego P <andresam9@gmail.com>
2024-09-23 15:16:55 -0500

    Se crean scripts y Json para la simulacion de base de datos

---
+++ js/scriptHome.js
@@ -0,0 +1,21 @@
+
+
+
+//Creamos acceo a las otras paginas del proyecto con los botones Call-To-Action
+//Boton Registrarse
+$(document).ready(function() {
+    $('#register').on('click', function() {
+      window.location.href = 'Register.html';
+    });
+  });
+
+  //Boton Contactanos
+  $(document).ready(function() {
+    $('#contactanos').on('click', function() {
+      window.location.href = 'contactanos.html';
+    });
+  });
+
+  //Boton Cotizaya ... Proceso de construccion
+
+  
